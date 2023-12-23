00b60956655e766a45dc6199afb9747459894dac
euisuh <euisuh.jeong@gmail.com>
2023-12-23 21:31:53 +0000

    add other baords

--- app/sub/board.php
+++ app/sub/board.php
@@ -53,3 +53,70 @@ class Free extends \controller\Make_Controller {
     }
 
 }
+
+class Daangn extends \controller\Make_Controller {
+
+    public function init()
+    {
+        $this->layout()->category_key(8);
+        $this->layout()->head();
+        $this->layout()->view();
+        $this->layout()->foot();
+    }
+
+    public function make(){
+        $this->module();
+    }
+
+    public function module(){
+        $module = new \Module\Board\Make_Controller();
+        $module->set('id', 'daangn');
+        $module->run();
+    }
+
+}
+
+class Bamboo extends \controller\Make_Controller {
+
+    public function init()
+    {
+        $this->layout()->category_key(9);
+        $this->layout()->head();
+        $this->layout()->view();
+        $this->layout()->foot();
+    }
+
+    public function make(){
+        $this->module();
+    }
+
+    public function module(){
+        $module = new \Module\Board\Make_Controller();
+        $module->set('id', 'bamboo');
+        $module->run();
+    }
+
+}
+
+
+class QatarAirways extends \controller\Make_Controller {
+
+    public function init()
+    {
+        $this->layout()->category_key(10);
+        $this->layout()->head();
+        $this->layout()->view();
+        $this->layout()->foot();
+    }
+
+    public function make(){
+        $this->module();
+    }
+
+    public function module(){
+        $module = new \Module\Board\Make_Controller();
+        $module->set('id', 'qatarAirways');
+        $module->run();
+    }
+
+}
