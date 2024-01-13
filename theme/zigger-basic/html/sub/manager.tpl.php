0c975ae5887ec255d826b061bf5d635137c3d58a
euisuh <euisuh.jeong@gmail.com>
2024-01-13 08:04:30 +0000

    change image path of manager page

--- theme/zigger-basic/html/sub/manager.tpl.php
+++ theme/zigger-basic/html/sub/manager.tpl.php
@@ -6,13 +6,12 @@
     </h2>
 
     <div class="inner">
-
         <!-- <h2 class="sub-tit">코카</h2> -->
         <div class="inner">
             <div class="gui-warbox">
-                <img src="https://s3.amazonaws.com/korqar-zigger-bucket/manage/korqar.png" alt="KORQAR" width="200" height="200">
+                <img src="/data/manage/admin_korqar.png" alt="KORQAR" width="200" height="200">
                 <br><br><br>
-                <strong>코카 <i>@korqar @admin</i></strong>
+                <strong>코카 <i>@korqar</i></strong>
                 <p>여러분, 여기 코카에요! 저는 이곳의 귀여운 마스코트랍니다. 때로는 귀엽고 다정하게, 때로는 단호하게 웹사이트를 지켜보고 있어요. </p>
                 <p>항상 여러분의 행복과 안전을 위해 최선을 다해 운영진들을 굴리고 있으니, 언제든지 저에게 놀러와주세요! 🥳✨</p>
             </div>
@@ -22,9 +21,9 @@
         <!-- <h2 class="sub-tit">활발한 프로그래머</h2> -->
         <div class="inner">
             <div class="gui-warbox">
-                <img src="https://s3.amazonaws.com/korqar-zigger-bucket/manage/admin1.png" alt="Admin 1" width="200" height="200">
+                <img src="/data/manage/admin_codewiz.png" alt="Admin 1" width="200" height="200">
                 <br><br><br>
-                <strong>활발한 프로그래머 <i>@codewiz</i></strong>
+                <strong>활발한 프로그래머 <i>@admin</i></strong>
                 <p>안녕하세요! KORQAR의 활발한 프로그래머입니다. 웹사이트의 개발과 유지보수를 담당하며, 항상 사용자들에게 더 나은 경험을 제공하기 위해 노력합니다.</p>
                 <p>팀원들과 협력하여 웹사이트를 지속적으로 향상시키고, 다양한 활동과 이벤트에 즐겁게 참여하고 있습니다. 여러분과 함께 KORQAR 커뮤니티를 더욱 발전시켜 나가기를 기대합니다!</p>
             </div>
@@ -33,7 +32,7 @@
         <!-- <h2 class="sub-tit">고인물 디자이너</h2> -->
         <div class="inner">
             <div class="gui-warbox">
-                <img src="https://s3.amazonaws.com/korqar-zigger-bucket/manage/admin2.png" alt="Admin 2" width="200" height="200">
+                <img src="/data/manage/admin_oldartist.png" alt="Admin 2" width="200" height="200">
                 <br><br><br>
                 <strong>고인물 화가 <i>@oldartist</i></strong>
                 <p>안녕하세요. KORQAR의 디자이너로서, 사이트의 시각적인 디자인을 개선하고 사용자 경험을 향상시키기 위해 노력하고 있습니다. </p>
@@ -44,7 +43,7 @@
         <!-- <h2 class="sub-tit">고독한 익명</h2> -->
         <div class="inner">
             <div class="gui-warbox">
-                <img src="https://s3.amazonaws.com/korqar-zigger-bucket/manage/admin3.png" alt="Admin 3" width="200" height="200">
+                <img src="/data/manage/admin_richbaeksu.png" alt="Admin 3" width="200" height="200">
                 <br><br><br>
                 <strong>돈 많을 백수 <i>@richbaeksu</i></strong>
                 <p>웹사이트의 알파 테스트와 정책, 그리고 웹사이트의 방향성을 결정하는 역할을 담당하고 있습니다. </p>
