96e1de71d6a3b661d2a8c4fba2b35f4eb44a3179
euisuh <euisuh.jeong@gmail.com>
2024-01-20 10:25:52 +0000

    data init commit

---
+++ data/dbconn.set.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_ENGINE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'u161709331_admin');
+define('DB_PWD', 'Wjdkis15!');
+define('DB_NAME', 'u161709331_korqar');
+define('DB_PREFIX', 'ph_');
+define('DB_SPECIALCHARS', 1);
+define('DB_NL2BR', 1);
+
+date_default_timezone_set('Asia/Qatar');
+?>
---
+++ data/path.set.php
@@ -0,0 +1,6 @@
+<?php
+define('PH_DOMAIN', 'https://korqar.com');
+define('PH_DIR', '');
+define('PH_PATH', $_SERVER['DOCUMENT_ROOT'].PH_DIR);
+date_default_timezone_set('Asia/Qatar');
+?>
